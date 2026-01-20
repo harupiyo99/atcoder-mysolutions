3149bb2026c20af596729915412f6124a94da260
harupiyo-mech <harukigcm@gmail.com>
2026-01-21 08:33:43 +0900

    Create: abc166_b.cpp

---
+++ ABC/ABC166/abc166_b.cpp
@@ -0,0 +1,40 @@
+#include <iostream>   
+#include <vector>    
+#include <string>    
+#include <algorithm> 
+#include <map>       
+#include<queue>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+using ll = long long int;
+using vi = vector<int>;
+using vvi = vector<vector<int>>; 
+using msi = map<string, int>;
+
+int main()
+{
+    int N,K;
+    cin >> N >> K;
+    
+    vi d(K + 1,0);
+    vvi A(K + 1,vi(1,0));
+    vi sunuke(N + 1,0);
+    for (int i = 1;i <= K;i++) {
+        cin >> d[i];
+        for (int j = 1;j <= d[i];j++) {
+            int temp;
+            cin >> temp;
+            A[i].push_back(temp);
+            sunuke[A[i][j]]++;
+        }
+    }
+
+    int count = 0;
+    for (int i = 1;i <= N;i++) {
+        if (sunuke[i] == 0) count++;
+    }
+    cout << count << "\n";
+    return 0;
+}
