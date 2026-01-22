359e33edd74abfe85881adfb881de4106ee823e4
harupiyo-mech <harukigcm@gmail.com>
2026-01-23 06:49:38 +0900

    Create: abc096_a.cpp

---
+++ ABC/ABC096/abc096_a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>   
+#include <vector>    
+#include <string>    
+#include <algorithm> 
+#include <map>       
+#include<queue>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+using ll = long long int;
+using vi = vector<int>;
+using vvi = vector<vector<int>>; 
+using msi = map<string, int>;
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+
+    if (a > b) cout << a - 1;
+    else cout << a;
+    return 0;
+}
