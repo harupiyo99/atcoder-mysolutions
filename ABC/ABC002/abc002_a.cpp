d98cc8800112cd2229626cc2fef7cba915f406cd
harupiyo-mech <harukigcm@gmail.com>
2026-01-25 13:47:15 +0900

    Create: abc002_a.cpp

---
+++ ABC/ABC002/abc002_a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>   
+#include <vector>    
+#include <string>    
+#include <algorithm>
+#include <utility> 
+#include <map>       
+#include<queue>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+using ll = long long int;
+using vi = vector<int>;
+using vvi = vector<vector<int>>; 
+using msi = map<string, int>;
+
+int main()
+{
+    ll X,Y;
+    cin >> X >> Y;
+
+    if (X > Y) cout << X << "\n";
+    else cout << Y << "\n";
+    return 0;
+}
